e90d68b5925e20db661a27a80594b9566f515027
marcusungarette <marcusungarette@gmail.com>
2021-12-09 20:52:13 -0300

    Feat-Create Card component with styles

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,23 @@
+import { CardContainer } from './card-styles'
+
+interface CardProps {
+    width?: string,
+    children?: React.ReactNode;
+    height?: string,
+    noShadow?: boolean
+}
+
+const Card = ({
+  children,
+  width = '100%',
+  height = 'auto',
+  noShadow = false,
+}: CardProps) => {
+  return (
+    <CardContainer width={width} height={height} noShadow={noShadow}>
+      {children}
+    </CardContainer>
+  )
+}
+
+export default Card
