fc3bc50f0a02c5e74051a25d9ebcd9ef84413b50
marcusungarette <marcusungarette@gmail.com>
2021-12-12 11:04:17 -0300

    Feat-Bank Statement `div` beside the `div` Balance on screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -17,6 +17,13 @@ const Dashboard = () => {
             <h3 className='wallet'>{wallet.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
           </Card>
         </div>
+        <div>
+          <Card noShadow width='90%'>
+            <InlineTitle>
+              <h2 className='h2'>Extrato da conta</h2>
+            </InlineTitle>
+          </Card>
+        </div>
       </BodyContainer>
     </DashboardBackground>
   )
