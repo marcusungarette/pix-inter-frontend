0ca31a959ed1c7ddea0da5c6f3b42cec9e5bc535
marcusungarette <marcusungarette@gmail.com>
2021-12-12 10:57:57 -0300

    Feat-Dashboard Main container display wallet balance(static)

--- src/pages/Dashboard/dashboard.styles.ts
+++ src/pages/Dashboard/dashboard.styles.ts
@@ -8,3 +8,24 @@ export const DashboardBackground = styled.main`
     flex-direction: column;
     background-color: ${({ theme }) => theme.colors.backgroundLight};
 `
+export const BodyContainer = styled.main`
+    width: 80%;
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
+    > div {
+        flex: 1;
+        & > div {
+            margin-bottom: 20px;
+        }
+
+        &:nth-child(2){
+            display: flex;
+            justify-content: flex-end;
+        }
+    }
+`
+export const InlineTitle = styled.div`
+    display: flex;
+    width: 100%;
+`
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,23 @@
-import { DashboardBackground } from './dashboard.styles'
+import { DashboardBackground, BodyContainer, InlineTitle } from './dashboard.styles'
 
 import Header from '../../components/Header'
+import Card from 'components/Card'
 
 const Dashboard = () => {
+  const wallet = 5000
   return (
     <DashboardBackground>
       <Header />
+      <BodyContainer>
+        <div>
+          <Card noShadow width='90%'>
+            <InlineTitle>
+              <h2 className='h2'>Saldo atual</h2>
+            </InlineTitle>
+            <h3 className='wallet'>{wallet.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
+          </Card>
+        </div>
+      </BodyContainer>
     </DashboardBackground>
   )
 }
